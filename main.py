f4652301bfba4b64fb521ad25da50d2f01152a7e
amazingmist <sol4.amazingmist@gmail.com>
2018-08-24 23:10:45 +0700

    Adding code comment and optimal source code

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import re
 import os
 import json
-from pprint import pprint
-
-
 from difflib import SequenceMatcher
 
-def getSimilarityRatio(str1, str2):
+# return the similar ratio between two string.
+def getSimilarRatio(str1, str2):
     return SequenceMatcher(None, str1, str2).ratio()
 
+# read data from data.json to program
 filePath = os.path.dirname(__file__) + os.sep + "data.json"
 with open(filePath) as data_file:    
     data = json.load(data_file)
 
-
+# get input question from user
 question = input("Question: ")
-ratio = 0
+maxRatio = 0
 index = -1
+
+# find index of the most similar question with the user's question 
 for i in range(len(data)):
-    if getSimilarityRatio(data[i]["question"], question) > ratio:
-        ratio = getSimilarityRatio(data[i]["question"], question)
+    currentRatio = getSimilarRatio(data[i]["question"], question)
+    if currentRatio > maxRatio:
+        maxRatio = currentRatio
         index = i
-    
+
+# show the answer
 print("Answer: ", data[index]["answer"])
